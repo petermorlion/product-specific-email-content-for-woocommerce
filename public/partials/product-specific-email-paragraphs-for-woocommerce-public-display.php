c8fee2e441d70a3025493207009e39c7c9903dfd
Peter Morlion <peter.morlion@gmail.com>
2023-05-08 11:58:00 +0200

    Add generated code

---
+++ public/partials/product-specific-email-paragraphs-for-woocommerce-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://redstar.be
+ * @since      1.0.0
+ *
+ * @package    Product_Specific_Email_Paragraphs_For_Woocommerce
+ * @subpackage Product_Specific_Email_Paragraphs_For_Woocommerce/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
