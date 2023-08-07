f06341293b053c2927b4b83bd4cee22a8dbf936c
Peter Morlion <peter.morlion@gmail.com>
2023-08-07 18:03:19 +0200

    Remove enqueueing of public js and css because there is none

--- includes/class-product-specific-email-content-for-woocommerce.php
+++ includes/class-product-specific-email-content-for-woocommerce.php
@@ -173,9 +173,6 @@ class Product_Specific_Email_Content_For_Woocommerce {
 
 		$plugin_public = new Product_Specific_Email_Content_For_Woocommerce_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
 		$this->loader->add_action('woocommerce_email_order_details', $plugin_public, 'email_order_details', 10, 4);
 
 	}
