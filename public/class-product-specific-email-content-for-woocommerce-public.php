81a4fa2cdc860430e74ceb0279a91db92f671613
petermorlion <petermorlion@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2023-09-25 12:34:10 +0000

    Remove enqueueing of public js and css because there is none
    git-svn-id: https://plugins.svn.wordpress.org/product-specific-email-content-for-woocommerce/trunk@2971346 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/class-product-specific-email-content-for-woocommerce-public.php
+++ public/class-product-specific-email-content-for-woocommerce-public.php
@@ -55,52 +55,6 @@ class Product_Specific_Email_Content_For_Woocommerce_Public {
 	}
 
 	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Product_Specific_Email_Content_For_Woocommerce_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Product_Specific_Email_Content_For_Woocommerce_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/product-specific-email-content-for-woocommerce-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Product_Specific_Email_Content_For_Woocommerce_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Product_Specific_Email_Content_For_Woocommerce_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/product-specific-email-content-for-woocommerce-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
-	/**
 	 * Customize the order details in the email.
 	 *
 	 * @since    1.0.0
